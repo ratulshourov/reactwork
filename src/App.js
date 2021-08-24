8d776f3dd373e6027c13be5eecfd17a501e2a948
Ratul <ratuljibondhara@gmail.com>
2021-08-24 10:39:56 +0600

    Arrow Function

--- src/App.js
+++ src/App.js
@@ -4,14 +4,16 @@ import React from 'react';
 import Hello from './components/Hello';
 import About from './components/About';
 import Button from './components/Button';
+import Arrow from './components/Arrow';
 function App() {
   return (
     <div className="App">
-      Ratul Shourov
-      {5+9}
-      <Hello name="ratul" profession="Software Engineer"></Hello>
+      {/* Ratul Shourov
+      {5+9} */}
+      {/* <Hello name="ratul" profession="Software Engineer"></Hello>
       <About address="Dhaka"></About>
-      <Button></Button>
+      <Button></Button> */}
+      <Arrow></Arrow>
     </div>
   );
   
---
+++ src/components/Arrow.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Arrowwork=()=>{
+return(
+    <div>
+        <button>I Am From Arrow Function</button>
+    </div>
+);
+}
+
+export default Arrowwork;
