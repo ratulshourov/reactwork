0a50886880f53c6084c9320e9813147746c10424
Ratul <ratuljibondhara@gmail.com>
2021-08-25 11:31:52 +0600

    Multiple Input Form Field  Submit Work

--- src/components/Formwork.js
+++ src/components/Formwork.js
@@ -4,14 +4,17 @@ class Formwork extends Component {
   constructor() {
     super();
     this.state = {
-      name: ''
+      fname: '',
+      lname: '',
+      email: '',
+      address: ''
     }
   }
   eventhandler = (event) => {
 
     var formname = event.target.name;
     var formvalue = event.target.value;
-    this.setState({ name: formvalue });
+    this.setState({ [formname]: formvalue });
   }
   formsubmit=()=>{
     alert(this.state.name);
@@ -20,8 +23,14 @@ class Formwork extends Component {
     return (
       <div>
         <form onSubmit={this.formsubmit}>
-        Name:<h1>{this.state.name}</h1>
-          <input name="username" onChange={this.eventhandler} type="text" placeholder="Enter name" />
+        Name:<h1>{this.state.fname}</h1>
+        lname:<h1>{this.state.lname}</h1>
+        email:<h1>{this.state.email}</h1>
+        address:<h1>{this.state.address}</h1>
+          <input name="fname" onChange={this.eventhandler} type="text" placeholder="Enter name" /> <br></br>
+          <input name="lname" onChange={this.eventhandler} type="text" placeholder="Enter name" />  <br></br>
+          <input name="email" onChange={this.eventhandler} type="text" placeholder="Enter name" />  <br></br>
+          <input name="address" onChange={this.eventhandler} type="text" placeholder="Enter name" />  <br></br>
           <input type="submit" value="Submit" />
         </form>
       </div>
