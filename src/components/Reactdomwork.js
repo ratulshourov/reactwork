fe6a160a35e56278a9c8a9d68ac86b81bd80ab2e
Ratul <ratuljibondhara@gmail.com>
2021-08-25 10:45:34 +0600

    FindDomNode Work

--- src/components/Reactdomwork.js
+++ src/components/Reactdomwork.js
@@ -8,13 +8,17 @@ class Reactdomwork extends Component {
     var callback = function () {
       alert("Ok");
     }
+
     reactDom.render(element, container, callback);
+    var imgid=document.getElementById("imageid");
+    reactDom.findDOMNode(imgid).src="https://via.placeholder.com/150/0000FF/808080C/O https://placeholder.com/";
   }
   render() {
     return (
       <div>
         <button onClick={this.updatename}>Update Name By React DOm</button>
         <h1 id="name">My Name is Ratul</h1>
+        <img id="imageid" onClick={this.updatename} src="https://via.placeholder.com/300.png/09f/fffC/O https://placeholder.com/"></img>
       </div>
     );
   }
