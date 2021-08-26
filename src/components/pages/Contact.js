4e70dd6e984228984635e2d7a0bdbbb95ce99e29
Ratul <ratuljibondhara@gmail.com>
2021-08-26 11:04:58 +0600

    Axios Get Request

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
 import { Redirect } from 'react-router-dom';
+
+import axios from 'axios';
 class Contact extends Component {
+   componentDidMount(){
+       axios.get("https://restcountries.eu/rest/v2/all")
+       .then(response=>{
+           console.log(response.data);
+       })
+       .catch(error=>{
+        console.log(error);
+       })
+   }
     render() {
         
         return (
             <div>
                   <h1 style={{color:"red", fontSize:"45px"}}>Contact</h1>
+                
             </div>
         );
     }
